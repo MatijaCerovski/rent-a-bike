4ca21dd7b87d2337ac07dc920f7a368f1a0a9c49
MatijaCerovski <cerovskimatija@gmail.com>
2017-05-30 02:32:17 +0200

    dadana konfiguracija za spring security(in memory) nije jos dohvacanje usera iz baze. Maknut spring session ne treba ga uopce u pom.xml-u . Dodan controller za login i profil.

---
+++ src/main/java/com/rent/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.rent.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by Matija on 30.5.2017..
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login-error.html")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
+}
---
+++ src/main/java/com/rent/controller/ProfileController.java
@@ -0,0 +1,28 @@
+package com.rent.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+/**
+ * Created by Matija on 30.5.2017..
+ */
+
+@Controller
+@RequestMapping("/profile")
+public class ProfileController {
+
+    @GetMapping()
+    @PreAuthorize("hasAnyRole('ROLE_USER' , 'ROLE_ADMIN')")
+    public String openProfile(Model model, Principal principal){
+
+
+        return "profile";
+    }
+
+
+}
