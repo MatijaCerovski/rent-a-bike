3c1dc534d844cf2775955b6ad0f20da69a4176cf
MatijaCerovski <cerovskimatija@gmail.com>
2017-05-31 23:01:44 +0200

    maknut view resolver iz konfiguracije navodno to spring boot sam napravi.

--- src/main/java/com/rent/config/AppConfig.java
+++ src/main/java/com/rent/config/AppConfig.java
@@ -1,6 +1,9 @@
 package com.rent.config;
 
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 
 /**
@@ -22,16 +30,6 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableTransactionManagement
 public class AppConfig extends WebMvcConfigurerAdapter {
 
-
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/templates/");
-        viewResolver.setSuffix(".html");
-
-        return viewResolver;
-    }
 
 }
 
--- src/main/java/com/rent/controller/HomeController.java
+++ src/main/java/com/rent/controller/HomeController.java
@@ -6,8 +6,10 @@ import com.rent.service.shop.ShopService;
 import com.rent.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -48,7 +50,6 @@ public class HomeController {
         user = userService.findByUsername("matija");
 
         System.out.println("Dohvacanje usera test  " +user.getEmail());
-
 
         return "index";
     }
