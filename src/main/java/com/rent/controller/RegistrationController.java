ffe7d7f46169e1958b283931894197f20acd8428
Toni <akrizmani@tvz.hr>
2017-06-01 21:09:39 +0200

    Dodan view za registraciju, i controller koji bi ga trebao vracati. Ali me stalno preusmjerava na login

---
+++ src/main/java/com/rent/controller/RegistrationController.java
@@ -0,0 +1,23 @@
+package com.rent.controller;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
+/**
+ * Created by Toni on 01-Jun-17.
+ */
+@Controller
+public class RegistrationController {
+    @GetMapping("/registration")
+    public String openRegistration() {
+        return "registration";
+    }
+
+    @GetMapping("/registration-error")
+    public String openErrorRegistration(Model model) {
+        model.addAttribute("registrationError", true);
+        return "registration";
+    }
+}
