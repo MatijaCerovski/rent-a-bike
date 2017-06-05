81838c250f91246d36cb39cc6e1f750a839f408f
MatijaCerovski <cerovskimatija@gmail.com>
2017-06-05 21:11:32 +0200

    dodan validator za provjeru ispravnosti lozinke

---
+++ src/main/java/com/rent/dto/UserRegistrationDto.java
@@ -0,0 +1,7 @@
+package com.rent.dto;
+
+/**
+ * Created by Matija on 5.6.2017..
+ */
+public class UserDto {
+}
---
+++ src/main/java/com/rent/validation/PasswordMatchesValidator.java
@@ -0,0 +1,7 @@
+package com.rent.validation;
+
+/**
+ * Created by Matija on 5.6.2017..
+ */
+public class PasswordMatchesValidator {
+}
