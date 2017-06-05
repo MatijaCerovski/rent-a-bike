b57bced444e2c6ece7e79136f9d0a99cc2977d0a
MatijaCerovski <cerovskimatija@gmail.com>
2017-06-05 23:16:54 +0200

    Radi unos podataka u formi i slanje DTO objekta u kontroller. Radi i provjera ispravnosti unesenih lozinki i email-a.

--- src/main/java/com/rent/controller/RegistrationController.java
+++ src/main/java/com/rent/controller/RegistrationController.java
@@ -1,17 +1,26 @@
 package com.rent.controller;
 
 import com.rent.dto.UserRegistrationDto;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 /**
  * Created by Toni on 01-Jun-17.
  */
 @Controller
+@RequestMapping("/registration")
 public class RegistrationController {
 
-    @GetMapping("/registration")
+    @GetMapping
     public String openRegistration(Model model) {
 
         UserRegistrationDto userRegistrationDto = new UserRegistrationDto();
@@ -20,9 +29,17 @@ public class RegistrationController {
         return "registration";
     }
 
-    @GetMapping("/registration-error")
-    public String openErrorRegistration(Model model) {
-        model.addAttribute("registrationError", true);
-        return "registration";
+    @PostMapping
+    public ModelAndView registracijaKorisnika(@ModelAttribute("userRegistrationDto") @Valid UserRegistrationDto userRegistrationDto,
+                                              BindingResult bindingResult){
+
+
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("redirect:/registration");
+        } else {
+            return new ModelAndView("redirect:/login");
+            // return "redirect:/login";
+        }
     }
+
 }
