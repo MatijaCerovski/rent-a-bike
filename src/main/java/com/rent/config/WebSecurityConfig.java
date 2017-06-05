9f61c7b931cf9029375b6646e16301fe5c0e6c12
MatijaCerovski <cerovskimatija@gmail.com>
2017-06-05 22:37:49 +0200

    maknuta zabrana za sve requestove od sad se svi provjeravaju na metodi

--- src/main/java/com/rent/config/WebSecurityConfig.java
+++ src/main/java/com/rent/config/WebSecurityConfig.java
@@ -31,7 +31,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
