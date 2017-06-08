d8907a093e1dd9013877380ea9aca1ffa8bdc1f6
MihaMasic <mmasic@tvz.hr>
2017-06-08 19:02:31 +0200

    Login i logout gumbovi

--- src/main/java/com/rent/config/WebSecurityConfig.java
+++ src/main/java/com/rent/config/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created by Matija on 30.5.2017..
@@ -38,6 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/bikes")
                 .permitAll();
     }
 
